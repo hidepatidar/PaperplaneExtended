ee440462352ad7fb86633cea1c263dcf12e96b4f
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-09-30 22:18:19 +0530

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -20,17 +20,21 @@ DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/' \
 @register(outgoing=True, pattern="^.magisk$")
 async def magisk(request):
     """ magisk latest releases """
-    if not request.text[0].isalpha() and request.text[0] not in ("/", "#", "@",
-                                                                 "!"):
-        url = 'https://raw.githubusercontent.com/topjohnwu/magisk_files/master/'
-        releases = 'Latest Magisk Releases:\n'
-        for variant in ['stable', 'beta', 'canary_builds/canary']:
-            data = get(url + variant + '.json').json()
-            name = variant.split('_')[0].capitalize()
-            releases += f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | ' \
-                        f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | ' \
-                        f'[Uninstaller]({data["uninstaller"]["link"]})\n'
-        await request.edit(releases)
+    magisk_dict = {
+        "Stable":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/stable.json",
+        "Beta":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/master/beta.json",
+        "Canary":
+        "https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/release.json"
+    }
+    releases = 'Latest Magisk Releases:\n'
+    for name, release_url in magisk_dict.items():
+        data = get(release_url).json()
+        releases += f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | ' \
+                    f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | ' \
+                    f'[Uninstaller]({data["uninstaller"]["link"]})\n'
+    await request.edit(releases)
 
 
 @register(outgoing=True, pattern=r"^.device(?: |$)(\S*)")
